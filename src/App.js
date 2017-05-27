b1358b4cd11e26e3fe72f143921f7634f3422018
Ducksan Cho <ducktyper@gmail.com>
2017-05-27 12:18:04 +1200

    Add state to component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {on: true};
+
+    this.toggleOnOff = this.toggleOnOff.bind(this);
+  }
+
+  toggleOnOff() {
+    this.setState(prevState => ({
+      on: !prevState.on
+    }));
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,8 +23,9 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
+        <p className="App-intro" onClick={this.toggleOnOff}>
           To get started, edit <code>src/App.js</code> and save to reload.
+          <br/>{this.state.on ? 'ON' : 'OFF'}
         </p>
       </div>
     );
